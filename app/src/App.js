197949ee081751208a5e2f10c53dd60122f85e33
dev-cardenas <dev.cardenas@gmail.com>
2020-11-27 12:58:20 -0300

    up project react

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
-import logo from './balena_icon.svg';
-import './App.css';
+import Presentacion from './components/home';
 
 // https://github.com/electron/electron/issues/7300
 // We don't want to bundle electron in the webpack process so we use it's globally exposed require method.
-const electron = window.require('electron');
-const fs = electron.remote.require('fs');
-var files = fs.readdirSync('./');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to balena-electronjs-react</h2>
-        </div>
-        <div className="App-intro">
-          <p>
-          To get started, edit <code>src/App.js</code> and save to reload. You have access to all node apis. Here is a list of files in this directory.
-          </p>
-          {
-            files.map((file, key) => {
-              return <pre key={key}>{file}</pre>;
-            })
-          }
+          <div className="text-center">
+            <Presentacion />
+          </div>
         </div>
       </div>
     );
